1e501c8c617b6fced31da6c31f369016616c85c3
ONdraid <47297971+ONdraid@users.noreply.github.com>
2021-07-09 22:43:18 +0200

    webshot improvement ui changes

--- app/src/main/java/com/vomaon/edookit/widget/TimetableActivity.java
+++ app/src/main/java/com/vomaon/edookit/widget/TimetableActivity.java
@@ -1,13 +1,14 @@
 package com.vomaon.edookit.widget;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,21 +19,23 @@ public class TimetableActivity extends AppCompatActivity {
     SharedPreferences sharedPref;
     String data;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
 
         webView = findViewById(R.id.timetableWebView);
+        webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
         webView.getSettings().setBuiltInZoomControls(false);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
-        webView.setWebChromeClient(new WebChromeClient());
 
         sharedPref = this.getSharedPreferences("UserData", Context.MODE_PRIVATE);
         data = sharedPref.getString("timetableHtml", "");
-        webView.loadDataWithBaseURL(null, data, null, "UTF-8", null);
+        webView.loadDataWithBaseURL(null, data, "text/html", "UTF-8", null);
+        // webView.loadDataWithBaseURL(null, data, null, "UTF-8", null);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
--- app/src/main/python/gethtmltable.py
+++ app/src/main/python/gethtmltable.py
@@ -1,7 +1,4 @@
 from bs4 import BeautifulSoup
-from os.path import dirname, join
-from com.chaquo.python import Python
-from html2image import Html2Image
 import requests
 
 
@@ -10,8 +7,6 @@ def main(username, password, school_id):
     login_check_url = 'https://' + school_id + '.edookit.net/user/login?do=loginForm-checkCaptcha&loginForm-username=' + username
     timetable_url = 'https://' + school_id + '.edookit.net/timetable/?do=familyTimetable-resetFilter'
 
-    files_dir = str(Python.getPlatform().getApplication().getFilesDir())
-    file_name = join(dirname(files_dir), 'timetable.jpg')
 
     payload = {
         'username': username,
@@ -28,15 +23,14 @@ def main(username, password, school_id):
         r = s.get(timetable_url)
     try:
         soup = BeautifulSoup(r.text, 'html.parser')
-        soup.find(class_='timetable')['style'] = 'background-color:white;'
-        timetable_grab = '<html>\n<body>\n' + soup.find(class_='timetable').prettify() + '</body>\n</html>'
+        soup.find(class_='timetable-container')['style'] = 'background-color:white;max-width:900px;display: block'
+        for e in soup.select('script'):
+            e.extract()
+        timetable_grab = '<meta name="viewport" content="width=device-width, initial-scale=1">\n' + soup.find(class_='timetable-container').prettify().replace('\n        min-width: 1258px;', '')
         pass
     except:
         timetable_grab = "error"
         pass
-
-    # hti = Html2Image(output_path=files_dir)
-    # hti.screenshot(html_str=timetable_grab, size=(900, 350), save_as='timetable.jpg')
 
     return timetable_grab
 
