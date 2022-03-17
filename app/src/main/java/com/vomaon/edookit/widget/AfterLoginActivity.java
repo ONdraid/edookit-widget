33b032526dae7b67f47eaa42e81a98ef54f46378
ONdraid <47297971+ONdraid@users.noreply.github.com>
2022-03-17 08:02:22 +0100

    Enabled file upload & download

--- app/src/main/java/com/vomaon/edookit/widget/AfterLoginActivity.java
+++ app/src/main/java/com/vomaon/edookit/widget/AfterLoginActivity.java
@@ -1,19 +1,32 @@
 package com.vomaon.edookit.widget;
 
 import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.appwidget.AppWidgetManager;
+import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.webkit.WebSettingsCompat;
 import androidx.webkit.WebViewFeature;
@@ -22,6 +35,31 @@ public class AfterLoginActivity extends AppCompatActivity {
 
     Button button;
     WebView edookitWebView;
+    ProgressBar progressBar;
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        } else
+            Toast.makeText(this, "Failed to Upload Image", Toast.LENGTH_LONG).show();
+    }
 
     @SuppressLint({"SetJavaScriptEnabled", "RequiresFeature"})
     @Override
@@ -32,6 +70,7 @@ public class AfterLoginActivity extends AppCompatActivity {
         TextView welcomeTextView = findViewById(R.id.welcomeTextView);
         button = findViewById(R.id.signOutButton);
         edookitWebView = findViewById(R.id.edookitWebView);
+        progressBar = findViewById(R.id.progressBar3);
 
         int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
         if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
@@ -47,10 +86,76 @@ public class AfterLoginActivity extends AppCompatActivity {
         String welcomeText = getString(R.string.welcome) + " " + fullname + "!";
         welcomeTextView.setText(welcomeText);
 
-        edookitWebView.setWebViewClient(new WebViewClient());
         edookitWebView.getSettings().setJavaScriptEnabled(true);
+        edookitWebView.getSettings().setAllowFileAccess(true);
+        edookitWebView.getSettings().setAllowContentAccess(true);
+        edookitWebView.setWebViewClient(new WebViewClient());
+
+        edookitWebView.setWebChromeClient(new WebChromeClient()
+        {
+            // For 3.0+ Devices
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+
+            // For Lollipop 5.0+ Devices
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+            {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try
+                {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e)
+                {
+                    uploadMessage = null;
+                    Toast.makeText(AfterLoginActivity.this, "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+            {
+                mUploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+        });
         String schoolIDStr = sharedPref.getString("schoolID", "");
         edookitWebView.loadUrl("https://" + schoolIDStr + ".edookit.net/user/login");
+
+        edookitWebView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
 
         button.setOnClickListener(view -> {
             Context context = getApplicationContext();
@@ -74,6 +179,25 @@ public class AfterLoginActivity extends AppCompatActivity {
             Intent intent = new Intent(context, LoginActivity.class);
             startActivity(intent);
             finish();
+        });
+
+        edookitWebView.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
+            DownloadManager.Request request = new DownloadManager.Request(
+                    Uri.parse(url));
+            request.setMimeType(mimeType);
+            String cookies = CookieManager.getInstance().getCookie(url);
+            request.addRequestHeader("cookie", cookies);
+            request.addRequestHeader("User-Agent", userAgent);
+            request.setDescription("Downloading File...");
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                            url, contentDisposition, mimeType));
+            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            dm.enqueue(request);
+            Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
         });
 
     }
--- app/src/main/java/com/vomaon/edookit/widget/MainActivity.java
+++ app/src/main/java/com/vomaon/edookit/widget/MainActivity.java
@@ -1,27 +1,37 @@
 package com.vomaon.edookit.widget;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPref;
+    Boolean canContinue = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sharedPref = this.getSharedPreferences("UserData", Context.MODE_PRIVATE);
 
-        Boolean loginStatus = sharedPref.getBoolean("loginStatus", false);
-        if (loginStatus.equals(false)) {
-            openLoginActivity();
-        } else {
-            Intent intent = new Intent(this, AfterLoginActivity.class);
-            startActivity(intent);
-            finish();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED) {
+                canContinue = false;
+                Log.d("permission", "permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 101);
+            }
+        }
+
+        if (canContinue) {
+            redirect();
         }
 
     }
@@ -31,4 +41,30 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    public void redirect() {
+        Boolean loginStatus = sharedPref.getBoolean("loginStatus", false);
+        if (loginStatus.equals(false)) {
+            openLoginActivity();
+        } else {
+            Intent intent = new Intent(this, AfterLoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 101) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                redirect();
+            } else {
+                Toast.makeText(this, "Please enable file permission or reinstall app", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
 }
